632e2048d0c61412d4f4e7fc517da49d99cc385f
Brenden Davidson <brenden.davidson@audonomic.com>
2022-04-06 08:44:02 -0500

    Initial Commit

---
+++ Ray Tracer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ray Tracer
+//
+//  Created by Brenden Davidson on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ray Tracer/Ray_TracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ray_TracerApp.swift
+//  Ray Tracer
+//
+//  Created by Brenden Davidson on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Ray_TracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
